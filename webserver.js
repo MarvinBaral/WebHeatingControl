dd44560cfb7eb9eadf6a903c3f091bffb99e50d8
Marvin Baral (rpi3-1) <github@marvinbaral.de>
2016-05-16 15:28:17 +0000

    Outsourced css and html completely

--- webserver.js
+++ webserver.js
@@ -5,26 +5,34 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const https = require('https');
+//const https = require('https');
 const bodyParser = require('body-parser');
 const fs = require('fs'); //file-system
 const exec = require('child_process').exec;
 const rootDir = '/root/heating/'
 const fileHeader = rootDir + 'header.html';
 const fileFooter = rootDir + 'footer.html';
-const assemblePage = function (html) {
-	var content = fs.readFileSync(fileHeader, 'utf-8') + html + fs.readFileSync(fileFooter, 'utf-8');
+const fileIndex = rootDir + 'index.html';
+const assemblePage = function (fileToEmbed) {
+	var content = fs.readFileSync(fileHeader, 'utf-8') + fs.readFileSync(fileToEmbed, 'utf-8') + fs.readFileSync(fileFooter, 'utf-8');
 	return content;
 };
 
-var html = '<h1>Test page</h1>\n'
 
+//the normal webserver stuff
+//====================================================
 
-app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
 	res.contentType('text/html');
-	res.send(assemblePage(html));
+	res.send(assemblePage(fileIndex));
+})
+
+app.get('/*.css', function (req, res) {
+	res.contentType('text/css');
+	var filename = req.path;
+	res.sendFile(rootDir + filename);
 })
 
 http.createServer(app).listen(80);
