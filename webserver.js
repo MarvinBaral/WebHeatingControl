b6bd331498ff4b770b64afb897e96eb61ada1bad
Marvin Baral (rpi3-1) <github@marvinbaral.de>
2016-05-16 12:54:03 +0000

    Added html-Header and Footer

--- webserver.js
+++ webserver.js
@@ -9,14 +9,22 @@ const https = require('https');
 const bodyParser = require('body-parser');
 const fs = require('fs'); //file-system
 const exec = require('child_process').exec;
+const rootDir = '/root/heating/'
+const fileHeader = rootDir + 'header.html';
+const fileFooter = rootDir + 'footer.html';
+const assemblePage = function (html) {
+	var content = fs.readFileSync(fileHeader, 'utf-8') + html + fs.readFileSync(fileFooter, 'utf-8');
+	return content;
+};
 
-var html = '<h1>Test page</h1>'
+var html = '<h1>Test page</h1>\n'
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
 	res.contentType('text/html');
-	res.send(html);
+	res.send(assemblePage(html));
 })
 
 http.createServer(app).listen(80);
